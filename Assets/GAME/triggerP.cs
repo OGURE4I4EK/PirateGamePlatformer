fedcd514890d35796a7d556a3f18d4a62e64e6e1
itcube22 <104897682+itcube22@users.noreply.github.com>
2026-06-09 13:32:15 +0700

    asddas

--- Assets/GAME/triggerP.cs
+++ Assets/GAME/triggerP.cs
@@ -2,13 +2,13 @@ using UnityEngine;
 
 public class triggerP : MonoBehaviour
 {
-    public Rigidbody2D enemy;
+    public GameObject enemy;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("Player"))
         {
-            enemy.simulated = true;
+            enemy.isStatic = false;
         }
     }
 }
